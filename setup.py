665c70f445f62b2a7113682bd421a311592262d6
oh-yes-0-fps <55303619+oh-yes-0-fps@users.noreply.github.com>
2025-09-26 13:12:11 -0500

    Fix syntax error in package_dir declaration

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="underautomation",
     version="1.2.0",
-    package_dir{"": "src"},
+    package_dir={"": "src"},
     packages=find_packages(where="src", include=["underautomation*"]),
     py_modules=["underautomation"]
 )
